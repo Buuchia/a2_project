f0bfbc5ebe6b1f2adf1333f70a19c03fd1977465
buuchia <buuchi.mach@gmail.com>
2024-05-04 18:18:51 +1000

    run code

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 //setting up the style for the body of document
 document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
-document.body.style.backgroundColor = 'pink'
+document.body.style.backgroundColor = 'black'
 
 //creating a canvas element
 const cnv = document.getElementById (`cnv_element`)
@@ -26,9 +26,27 @@ class Symbol {
 
     }
 
-    //define a function to randomize and draw current characters to the canvas at a specific location
-    draw() {
+    //define a function to randomize current character and draw it to the canvas at a specific location
+    //every time the method is called, it will choose a random symbol
+    draw(context) {
 
+        //charAt() can be called on string data type,
+        //which takes a single 'index' argument and returns a new string 
+        //containing only that one character located at that specific offset of the string
+        //in charAt bracket is a random number between 0 and this,chars.length, no decimal
+        //so this.text will have a random character from the this.chars string
+        this.text = this.chars.charAt(Math.floor (Math.random() * this.chars.length))
+
+        context.fillStyle = '#0aff0a'
+
+        //make character sit next to and below each other
+        context.fillText(this.text, this.x * this.fontSize, this.y * fontSize)
+
+        if (this.y * this.fontSize > this.canvasHeight) {
+            this.y = 0
+        } else {
+            this.y += 1
+        }
 
     }
 }
@@ -38,9 +56,11 @@ class Effect {
     constructor(canvasWidth, canvasHeight) {
         this.canvasWidth = canvasWidth
         this.canvasHeight = canvasHeight
-        this.fontsize = 25
+        this.fontSize = 25
         this.columns = this.canvasWidth / this.fontsize
         this.symbols = [] //empty array to store symbol objects
+        this.#init() //calling private method
+        console.log (this.symbols)
     }
 
     //making the init method private by starting with #
@@ -51,22 +71,29 @@ class Effect {
     //inside class Effect's constructor
 
     #init(){
+
         //for loop will run once for each column
         //each time it will fill that index in symbol array 
         //with an instance of Symbol class
-        for (let i = 0; i < this.columns.length; i++){
-            this.symbols[i] = new Symbol
+        for (let i = 0; i < this.columns; i++){
+            this.symbols[i] = new Symbol (i, 0, this.fontSize, this.canvasHeight)
         }
 
     }
 }
 
+//declaring effect variable
+const effect = new Effect (cnv.width, cnv.height)
+
 //define a custom function to draw the rain effect
 //60 times per second
-
 function animate(){
-
+    ctx.font = effect.fontSize + 'px monospace'
+    effect.symbols.forEach(symbol => symbol.draw(ctx))
+    requestAnimationFrame(animate)
 }
+
+animate()
 
 // const draw_frame = () => {
 //    ctx.fillStyle = `turquoise`
