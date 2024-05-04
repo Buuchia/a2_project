92c1aad3d527047c11e3eb95121612675ed60dd6
buuchia <buuchi.mach@gmail.com>
2024-05-04 16:02:03 +1000

    rename html title

--- public/script.js
+++ public/script.js
@@ -7,16 +7,22 @@ cnv.height = innerHeight
 
 const ctx = cnv.getContext (`2d`)
 
-const draw_frame = () => {
-   ctx.fillStyle = `turquoise`
-   ctx.fillRect (0, 0, innerWidth, innerHeight)
 
-   requestAnimationFrame (draw_frame)
-}
 
-draw_frame ()
 
-window.onresize = () => {
-   cnv.width = innerWidth
-   cnv.height = innerHeight   
-}
+
+
+
+// const draw_frame = () => {
+//    ctx.fillStyle = `turquoise`
+//    ctx.fillRect (0, 0, innerWidth, innerHeight)
+
+//    requestAnimationFrame (draw_frame)
+// }
+
+// draw_frame ()
+
+// window.onresize = () => {
+//    cnv.width = innerWidth
+//    cnv.height = innerHeight   
+// }
