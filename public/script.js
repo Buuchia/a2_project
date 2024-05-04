5ea55b2ff1123e32526ca22a6e0b22fc10b409eb
buuchia <buuchi.mach@gmail.com>
2024-05-04 16:46:05 +1000

    setting up classes

--- public/script.js
+++ public/script.js
@@ -1,17 +1,42 @@
 document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
+document.body.style.backgroundColor = 'pink'
 
+//create a canvas element
 const cnv = document.getElementById (`cnv_element`)
-cnv.width = innerWidth
-cnv.height = innerHeight
+cnv.width = window.innerWidth
+cnv.height = window.innerHeight
 
+//get canvas context
 const ctx = cnv.getContext (`2d`)
 
+//create and draw individual symbol objects
+//that make up the rain effect
+class Symbol {
+    constructor() {
 
+    }
+    draw() {
 
+    }
+}
 
+//Main wrapper for entire rain effect
+class Effect {
+    constructor(canvasWidth, canvasHeight) {
 
+    }
+    #init(){
 
+    }
+}
+
+//define a custom function to draw the rain effect
+//60 times per second
+
+function animate(){
+
+}
 
 // const draw_frame = () => {
 //    ctx.fillStyle = `turquoise`
