a12f84d53a3efb0c6aed50d6320bf491c718b2dd
buuchia <buuchi.mach@gmail.com>
2024-05-05 00:55:59 +1000

    add c2minjs

---
+++ public/script_below.js
@@ -0,0 +1,16 @@
+// //setting up the style for the body of document
+// document.body.style.margin   = 0
+// document.body.style.overflow = `hidden`
+// document.body.style.backgroundColor = 'blue'
+
+// //creating a canvas element
+// const cnv = document.getElementById (`c2`)
+
+
+// //getting a good size of canvas
+// cnv.width = window.innerWidth
+// cnv.height = window.innerHeight
+
+// //getting canvas context
+// const ctx = cnv.getContext (`2d`)
+
