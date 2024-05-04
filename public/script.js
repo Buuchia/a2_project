334ed7ae8e9bba03b680b8280de151c069cc9177
buuchia <buuchi.mach@gmail.com>
2024-05-04 17:49:53 +1000

    ADD CHAR

--- public/script.js
+++ public/script.js
@@ -16,10 +16,19 @@ const ctx = cnv.getContext (`2d`)
 //class Symbol creates and draws individual symbol objects
 //that make up the rain effect
 class Symbol {
-    constructor() {
+    constructor(x, y, fontSize, canvasHeight) {
+        this.chars = 'アァカサタナハマヤャラワガザダバパイィキシチニ☀☁❆WELCOMETOMYZANYWORLD❅❄ヒミリヰギジヂビピウゥクスツヌフムユュルLETSBEMORECHAOTICグズブヅプ♔♕♖♗♘♙エェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン'
+        this.x = x
+        this.y = y
+        this.fontSize = fontSize
+        this.text = ''
+        this.canvasHeight = canvasHeight
 
     }
+
+    //define a function to randomize and draw current characters to the canvas at a specific location
     draw() {
+
 
     }
 }
